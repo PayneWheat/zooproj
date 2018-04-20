a5d1e986aa05ff74434a2972b0f88d8f092a006a
Andrew Walker <andrewg.walker@outlook.com>
2018-04-20 13:38:08 -0500

    Do actual authentication with db, only allow actual employees to login using their first name

--- zooproject/Pages/Login.cshtml.cs
+++ zooproject/Pages/Login.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -9,12 +10,25 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace zooproject.Pages
 {
     public class LoginModel : PageModel
     {
         public bool login_failed = false;
+        IConfiguration _config;
+        Database database;
+        string connection_string;
+
+        public LoginModel(IConfiguration iconfiguration, Database ZooDatabase)
+        {
+            // Get database connection string from appsettings.Development.json
+            _config = iconfiguration;
+            database = ZooDatabase;
+            connection_string = _config.GetSection("Data").GetSection("ConnectionString").Value;
+
+        }
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -36,18 +50,41 @@ namespace zooproject.Pages
             {
                 login_failed = true;
             }
-            if(login_failed)
+
+            database.connect();
+            SqlCommand cmd = new SqlCommand("SELECT id FROM EMPLOYEE WHERE Fname='" + userName + "'",
+                database.Connection);
+            SqlDataReader reader;
+             reader = cmd.ExecuteReader();
+
+            bool user_found = false;
+            while (reader.Read())
+            {
+                user_found = true;
+            }
+            cmd.Dispose();
+            reader.Close();
+            database.disconnect();
+
+            if (!user_found)
+                login_failed = true;
+
+
+            if (login_failed)
                 return Redirect("/LoginError");
+            else
+            {
 
-            var claims = new List<Claim> {
-                new Claim(ClaimTypes.Name, userName, ClaimValueTypes.String, "Manager")
-            };
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, userName, ClaimValueTypes.String, "Manager")
+                };
 
-            var userIdentity = new ClaimsIdentity(claims, "Passport");
+                var userIdentity = new ClaimsIdentity(claims, "EmployeeLogin");
 
-            var userPrincipal = new ClaimsPrincipal(userIdentity);
+                var userPrincipal = new ClaimsPrincipal(userIdentity);
 
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+            }
             return Redirect("./Employee_Section/");
         }
     }
