6a6d369ef2b9f5889f8fccb49c89f9f358646851
Andrew Walker <andrewg.walker@outlook.com>
2018-04-23 10:48:52 -0500

    Allow anonymous access to error page

--- zooproject/Startup.cs
+++ zooproject/Startup.cs
@@ -43,6 +43,7 @@ namespace zooproject
                 options.Conventions.AuthorizeFolder("/");
                 options.Conventions.AllowAnonymousToPage("/Index");
                 options.Conventions.AllowAnonymousToPage("/Login");
+                options.Conventions.AllowAnonymousToPage("/Error");
                 options.Conventions.AllowAnonymousToPage("/LoginError");
                 options.Conventions.AllowAnonymousToPage("/AttractionInfo");
                 options.Conventions.AllowAnonymousToPage("/ProductCatalog");
